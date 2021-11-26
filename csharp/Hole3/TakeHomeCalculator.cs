1133e2ee3f2d7909531ba7fd6d64f279ea98fd72
jvaquero <jvaquero@aidacanarias.com>
2021-11-26 07:33:37 +0000

    Hole 3 completed: 7 points

--- csharp/Hole3/TakeHomeCalculator.cs
+++ csharp/Hole3/TakeHomeCalculator.cs
@@ -21,17 +21,9 @@ namespace Hole3
 
             foreach (Money next in monies)
             {
-                if (!next.currency.Equals(total.currency))
-                {
-                    throw new Incalculable();
-                }
+                total = total.Plus(next);
             }
-
-            foreach (Money next in monies)
-            {
-                total = new Money(total.value + next.value, next.currency);
-            }
-
+            
             Double amount = total.value * (percent / 100d);
             Money tax = new Money(Convert.ToInt32(amount), first.currency);
 
